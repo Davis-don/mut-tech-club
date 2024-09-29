5e5a8b5ac3e7d811e9cb3e647cab67b7416c3947
Davis_the_don <davismugoikou@gmail.com>
2024-09-29 14:18:27 +0300

    added leadership hero section

---
+++ src/Components/Leadership/Leadershiphero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Leadershiphero.css'
+import peopleInOffice from  '../../assets/mapbox-pzFzd1AD8Ow-unsplash.jpg'
+function Leadershiphero() {
+  return (
+    <div className='Overall-leadership-hero-section'>
+        <img className='leadership-about-image' alt='image of people in office' src={peopleInOffice}/>
+        <div className="leadership-hero-opacity-regulator"> </div>
+        <div className="on-image-leadership-text">
+            <h2 className='text-light leadership-hero-heading'>Our Leadership</h2>
+            <p className='text-light hero-leadership-paragraph'>Our very able and competent leaders</p>
+        </div>
+        </div>
+  )
+}
+
+export default Leadershiphero
--- src/Components/Pages/Leadership.jsx
+++ src/Components/Pages/Leadership.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Leadershiphero from '../Leadership/Leadershiphero'
 
 function Leadership() {
   return (
     <div className='overall-leadership-page'>
-        <h1 className='text-light'>This is leadership page</h1>
+        <div className="about-hero-section">
+        <Leadershiphero/>
+        </div>
+        <div className="coming-onboard-section">
+<h1 className='text-light'>Comming-onboard-section</h1>
+        </div>
+        <div className="meet-team-section">
+
+        </div>
         </div>
   )
 }
