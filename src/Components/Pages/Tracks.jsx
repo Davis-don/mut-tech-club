5450f7d94a81e3a36ec4f1952017dd21549e6e2e
Davis_the_don <davismugoikou@gmail.com>
2024-10-01 01:50:45 +0300

    modified

--- src/Components/Pages/Tracks.jsx
+++ src/Components/Pages/Tracks.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Trackshero from '../Tracks/Trackshero/Trackshero'
 import Trackscards from '../Tracks/Trackscard/Trackscards'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Tracksform from '../Tracks/Applyform/Tracksform'
-// import Tracksform from '../Tracks/Applyform/tracksform'
+// import Tracksform from '../Tracks/Applyform/Tracksform'
+
 
 function Tracks() {
   return (
     <div className='overall-tracks-page bg-light'>
       <Trackshero/>
       <Trackscards/>
-      <Tracksform/>
+      {/* <Tracksform/> */}
       </div>
   )
 }
